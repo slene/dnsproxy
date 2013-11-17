088ece420dbadeee4a47571c5bff6ba3cbc7fb51
slene <vslene@gmail.com>
2013-11-17 11:51:20 +0800

    update

--- dnsproxy.go
+++ dnsproxy.go
@@ -6,7 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/miekg/dns"
-	goCache "github.com/pmylund/go-cache"
+	"github.com/pmylund/go-cache"
 	"log"
 	"net"
 	"os"
@@ -20,23 +20,24 @@ import (
 )
 
 var (
-	dnss    = flag.String("dns", "192.168.2.1:53,8.8.8.8:53,8.8.4.4:53", "dns address, use `,` as sep")
+	dnss    = flag.String("dns", "192.168.2.1:53:udp,8.8.8.8:53:udp,8.8.4.4:53:udp,8.8.8.8:53:tcp,8.8.4.4:53:tcp", "dns address, use `,` as sep")
 	local   = flag.String("local", ":53", "local listen address")
 	debug   = flag.Int("debug", 0, "debug level 0 1 2")
-	cache   = flag.Bool("cache", true, "enable go-cache")
-	expire  = flag.Int64("expire", 3600, "default cache expire time")
+	encache = flag.Bool("cache", true, "enable go-cache")
+	expire  = flag.Int64("expire", 3600, "default cache expire seconds, -1 means use doamin ttl time")
 	file    = flag.String("file", filepath.Join(path.Dir(os.Args[0]), "cache.dat"), "cached file")
 	ipv6    = flag.Bool("6", false, "skip ipv6 record query AAAA")
 	timeout = flag.Int("timeout", 200, "read/write timeout")
 
-	client *dns.Client
+	clientTCP *dns.Client
+	clientUDP *dns.Client
 
-	DEBUG int
-	CACHE bool
+	DEBUG   int
+	ENCACHE bool
 
-	DNS []string
+	DNS [][]string
 
-	conn *goCache.Cache
+	conn *cache.Cache
 
 	saveSig = make(chan os.Signal)
 )
@@ -64,6 +65,9 @@ func intervalSaveCache() {
 				switch sig {
 				case syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
 					os.Exit(0)
+				case syscall.SIGHUP:
+					log.Println("recv SIGHUP clear cache")
+					conn.Flush()
 				}
 			case <-time.After(time.Second * 60):
 				save()
@@ -75,18 +79,23 @@ func intervalSaveCache() {
 func init() {
 	flag.Parse()
 
-	CACHE = *cache
+	ENCACHE = *encache
 	DEBUG = *debug
 
 	runtime.GOMAXPROCS(runtime.NumCPU()*2 - 1)
 
-	client = new(dns.Client)
-	client.Net = "tcp"
-	client.ReadTimeout = time.Duration(*timeout) * time.Millisecond
-	client.WriteTimeout = time.Duration(*timeout) * time.Millisecond
+	clientTCP = new(dns.Client)
+	clientTCP.Net = "tcp"
+	clientTCP.ReadTimeout = time.Duration(*timeout) * time.Millisecond
+	clientTCP.WriteTimeout = time.Duration(*timeout) * time.Millisecond
 
-	if CACHE {
-		conn = goCache.New(time.Second*time.Duration(*expire), time.Second*60)
+	clientUDP = new(dns.Client)
+	clientUDP.Net = "udp"
+	clientUDP.ReadTimeout = time.Duration(*timeout) * time.Millisecond
+	clientUDP.WriteTimeout = time.Duration(*timeout) * time.Millisecond
+
+	if ENCACHE {
+		conn = cache.New(time.Second*time.Duration(*expire), time.Second*60)
 		conn.LoadFile(*file)
 		intervalSaveCache()
 	}
@@ -96,11 +105,20 @@ func init() {
 		if s == "" {
 			continue
 		}
-		_, err := net.ResolveTCPAddr("tcp", s)
-		if err != nil {
-			log.Fatalf("wrong dns address %s\n", s)
+		dns := s
+		proto := "udp"
+		parts := strings.Split(s, ":")
+		if len(parts) > 2 {
+			dns = strings.Join(parts[:2], ":")
+			if parts[2] == "tcp" {
+				proto = "tcp"
+			}
 		}
-		DNS = append(DNS, s)
+		_, err := net.ResolveTCPAddr("tcp", dns)
+		if err != nil {
+			log.Fatalf("wrong dns address %s\n", dns)
+		}
+		DNS = append(DNS, []string{dns, proto})
 	}
 
 	if len(DNS) == 0 {
@@ -172,7 +190,7 @@ func proxyServe(w dns.ResponseWriter, req *dns.Msg) {
 	key = toMd5(req.String())
 	req.Id = id
 
-	if CACHE {
+	if ENCACHE {
 		if reply, ok := conn.Get(key); ok {
 			data, _ = reply.([]byte)
 		}
@@ -194,14 +212,20 @@ func proxyServe(w dns.ResponseWriter, req *dns.Msg) {
 		}
 	}
 
-	for i, dns := range DNS {
+	for i, parts := range DNS {
+		dns := parts[0]
+		proto := parts[1]
 		tried = i > 0
 		if DEBUG > 0 {
 			if tried {
-				log.Printf("id: %5d try: %v %s\n", id, query, dns)
+				log.Printf("id: %5d try: %v %s %s\n", id, query, dns, proto)
 			} else {
-				log.Printf("id: %5d resolve: %v %s\n", id, query, dns)
+				log.Printf("id: %5d resolve: %v %s %s\n", id, query, dns, proto)
 			}
+		}
+		client := clientUDP
+		if proto == "tcp" {
+			client = clientTCP
 		}
 		m, _, err = client.Exchange(req, dns)
 		if err == nil && len(m.Answer) > 0 {
@@ -225,13 +249,20 @@ func proxyServe(w dns.ResponseWriter, req *dns.Msg) {
 			_, err = w.Write(data)
 
 			if err == nil {
-				if CACHE {
+				if ENCACHE {
 					m.Id = 0
 					data, _ = m.Pack()
-					conn.Set(key, data, 0)
+					ttl := 0
+					if len(m.Answer) > 0 {
+						ttl = int(m.Answer[0].Header().Ttl)
+						if ttl < 0 {
+							ttl = 0
+						}
+					}
+					conn.Set(key, data, time.Second*time.Duration(ttl))
 					m.Id = id
 					if DEBUG > 0 {
-						log.Printf("id: %5d cache: CACHED %v\n", id, query)
+						log.Printf("id: %5d cache: CACHED %v TTL %v\n", id, query, ttl)
 					}
 				}
 			}
